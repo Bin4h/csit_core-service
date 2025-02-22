22399fc4b61951ea5b2a875ec26d6bfc7abec523
Mariia Schmidt <snowz0230@gmail.com>
2025-02-22 12:03:54 +0400

    1lab

--- src/main/java/com/core_service/core_service/CoreServiceApplication.java
+++ src/main/java/com/core_service/core_service/CoreServiceApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CoreServiceApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(CoreServiceApplication.class, args);
 	}
 
